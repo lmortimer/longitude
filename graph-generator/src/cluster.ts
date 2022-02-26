a91da4b4dbe9e4cb961602d50b8eeef8f0eeb221
Logan Mortimer <logan@isthisit.nz>
2022-02-26 11:39:36 +1100

    Add default cluster checK

--- graph-generator/src/cluster.ts
+++ graph-generator/src/cluster.ts
@@ -1,13 +1,21 @@
 import { readFileSync } from 'fs';
 import { Cluster } from 'types';
 
+const DEFAULT_CLUSTER_KEY = '0';
+
 export const loadClusters = (fileName: string): Array<Cluster> => {
-    return JSON.parse(readFileSync(fileName, 'utf-8'));
+    const clusters = JSON.parse(readFileSync(fileName, 'utf-8')) as Array<Cluster>;
+
+    if (!clusters.find( cluster => cluster.key === DEFAULT_CLUSTER_KEY)) {
+        throw Error(`clusters.jsons requires a default cluster with key = '${DEFAULT_CLUSTER_KEY}'`);
+    } 
+
+    return clusters;
 }
 
 export const clusterForNodeName = (name: string, clusters: Array<Cluster>) => {
     // default to cluster 0
-    let cluster = '0';
+    let cluster = DEFAULT_CLUSTER_KEY;
 
     clusters.forEach((clusterItem: any) => {
         clusterItem.paths.forEach((clusterPath: string) => {
